279a9c8c66b693ade22e4806dcba7399dbe81e6f
Siddhi7 <10014449@sbstudents.org>
2018-11-27 16:57:10 -0500

    No Duplicate #s
    Added a method using 3 for loops to check for duplicate numbers in list C, set it to 11, and then remove all of them. Also cleaned up the formatting

--- ArrayListPractice3.java
+++ ArrayListPractice3.java
@@ -5,11 +5,11 @@ public class ArrayListPractice3{
 		ArrayList<Integer> listB = new ArrayList<>();
 		ArrayList<Integer> listC = new ArrayList<>();
 
-		System.out.println(FillList(listA));
-		System.out.println(FillList(listB));
-		System.out.println(CombineList(listA,listB,listC));
-		System.out.println(PrintList(listC));
-
+		System.out.println("List A: " + FillList(listA));
+		System.out.println("List B: " + FillList(listB));
+		System.out.println("List A + List B = ListC: " + CombineList(listA,listB,listC));
+		System.out.println("List C: " + PrintList(listC));
+		System.out.println("No Duplicate #s - List C: " + RemoveDuplicate(listC));
 	}
 	public static ArrayList<Integer> FillList(ArrayList<Integer> list){
 		for(int x = 0; x < 10; x++){
@@ -33,5 +33,18 @@ public class ArrayListPractice3{
 				output += (listC.get(x) + ", ");
 		}
 		return output;
+	}
+	public static ArrayList<Integer> RemoveDuplicate(ArrayList<Integer> listC){
+		for(int x = listC.size()-1; x > 0; x--){
+			for(int a = 0; a < x; a++){
+				if (listC.get(x) == listC.get(a))
+					listC.set(x, 11);
+			}
+			for(int b = listC.size()-1; b > 0; b--){
+				if (11 == listC.get(b))
+					listC.remove(b);
+			}
+		}
+		return listC;
 	}
 }
