62ff375a8b0bbeb2010bbb5826f264af80705b56
Siddhi7 <10014449@sbstudents.org>
2018-11-27 16:10:12 -0500

    Print Without Brackets
    Used a String method to print out list C with commas and spaces, but no brackets. Also, moved the initialization of list C into the main method.

--- ArrayListPractice3.java
+++ ArrayListPractice3.java
@@ -3,10 +3,12 @@ public class ArrayListPractice3{
 	public static void main(String[]args){
 		ArrayList<Integer> listA = new ArrayList<>();
 		ArrayList<Integer> listB = new ArrayList<>();
+		ArrayList<Integer> listC = new ArrayList<>();
 
 		System.out.println(FillList(listA));
 		System.out.println(FillList(listB));
-		System.out.println(CombineList(listA,listB));
+		System.out.println(CombineList(listA,listB,listC));
+		System.out.println(PrintList(listC));
 
 	}
 	public static ArrayList<Integer> FillList(ArrayList<Integer> list){
@@ -15,14 +17,21 @@ public class ArrayListPractice3{
 		}
 		return list;
 	}
-	public static ArrayList<Integer> CombineList(ArrayList<Integer> listA, ArrayList<Integer> listB){
-		ArrayList<Integer> listC = new ArrayList<>();
+	public static ArrayList<Integer> CombineList(ArrayList<Integer> listA, ArrayList<Integer> listB,  ArrayList<Integer> listC){
 		for(int x = 0; x < listA.size(); x++)
 			listC.add(listA.get(x));
 		for(int x = 0; x < listB.size(); x++)
 			listC.add(listB.get(x));
 		return listC;
 	}
-
-
+	public static String PrintList(ArrayList<Integer> listC){
+		String output = "";
+		for(int x = 0; x < listC.size(); x++){
+			if (x == listC.size() - 1)
+				output += (listC.get(x));
+			else
+				output += (listC.get(x) + ", ");
+		}
+		return output;
+	}
 }
